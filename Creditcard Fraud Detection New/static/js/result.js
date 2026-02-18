b94b1c38f246fa975c2541c2c9fb9ce71cdedef3
Herlan Pio <125283599+HerlanPio@users.noreply.github.com>
2026-02-18 23:52:52 +0530

    Delete Creditcard Fraud Detection New directory

--- Creditcard Fraud Detection New/static/js/result.js
+++
@@ -1,5 +0,0 @@
-var str = document.getElementById("output").innerHTML;
-if (str == "VALID TRANSACTION")
-    document.getElementById("output").style.color = "green";
-else
-    document.getElementById("output").style.color = "red";
