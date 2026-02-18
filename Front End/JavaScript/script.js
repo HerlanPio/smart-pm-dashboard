c95f576e1619d3bd396ad0b3fbec0c5165514a6d
Herlan Pio <125283599+HerlanPio@users.noreply.github.com>
2026-02-18 23:53:03 +0530

    Delete Front End directory

--- Front End/JavaScript/script.js
+++
@@ -1,8 +0,0 @@
-function scrollToSection(sectionId) {
-    document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
-}
-
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Your message has been sent!');
-});
